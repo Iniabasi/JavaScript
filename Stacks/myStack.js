743e4d429d3e5b395292989af4ab999d8b5f6d43
Iniabasi <43305018+Iniabasi@users.noreply.github.com>
2021-12-29 19:33:40 -0500

    Update myStack.js
    Added pop, peek and size function

--- Stacks/myStack.js
+++ Stacks/myStack.js
@@ -17,7 +17,28 @@ var Stack = function(){
       this.count++;
     }
     
-
+    // Function Removes and Returns Value at the end of the stack
+    this.pop = function(){
+      if(this.count === 0){
+        return undefined;
+      }
+      
+      this.count--;
+      var result = this.storage[this.count];
+      delete this.storage[this.count];
+      return result;
+      
+    }
+    
+    // Function returns the element at the end of the stack without removing it
+    this.peek = function(){
+      return this.storage[this.count-1];
+    }
+    
+    // Function returns the size of the stack
+    this.size = function(){
+      return this.count;
+    }
   }
   
   
